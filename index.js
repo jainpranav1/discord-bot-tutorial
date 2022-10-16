72a3abd2aa27ed2fbb7a6063747afb810c29aafb
jainpranav1 <pranavji@tamu.edu>
2022-10-15 22:39:33 -0500

    Make your bot speak

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const { Client, GatewayIntentBits } = require('discord.js');
 // allows us to access the secrets in the .env file
 require("dotenv").config();
 
+// import uberduck.js
+// allows us to call the uberduck service
+const uberduck = require("./uberduck.js")
+
 // creating discord client
 // intents specify permissions of the discord bot
 const client = new Client({
     intents: [
+        GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
     ]
@@ -17,8 +22,25 @@ const client = new Client({
 // accessing our discord bot with its token (stored on the .env file)
 client.login(process.env.BOT_TOKEN);
 
-// print when bot is ready
+// runs when bot is ready
 client.on("ready", () => {
     console.log("Bot is ready");
 });
 
+// runs when a message is sent in a channel
+client.on("messageCreate", (msg) => {
+
+    // when bot sees a message of form: hey squidward, say "I am awesome"
+    // it will send a voice message stating: I am awesome
+    if (msg.content.startsWith("hey squidward, say")) {
+        let ind_quote_1 = 19;
+        let ind_quote_2 = msg.content.length - 1;
+        let reply = msg.content.substring(ind_quote_1, ind_quote_2);
+
+        uberduck.get_path_vt("squidward", reply).then((path) => {
+            msg.channel.send({
+                files: [path],
+            });
+        })
+    }
+})
---
+++ uberduck.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+require("dotenv").config();
+
+// gets uuid from voice and text
+async function get_uuid_vt(voice, text) {
+    const url = "https://api.uberduck.ai/speak";
+    const options = {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "uberduck-id": "anonymous",
+            "Content-Type": "application/json",
+            Authorization: `${process.env.AUTHORIZATION}`,
+        },
+        body: JSON.stringify({ voice: voice, pace: 1, speech: text }),
+    };
+
+    let res = await fetch(url, options);
+    let json = await res.json();
+
+    return json.uuid;
+}
+
+// gets path to voice file from uuid
+function get_path_u(uuid) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const url = `https://api.uberduck.ai/speak-status?uuid=${uuid}`;
+            const options = {
+                method: "GET",
+                headers: { Accept: "application/json" },
+            };
+
+            fetch(url, options)
+                .then((res) => res.json())
+                .then((json) => {
+                    if (json.path == null) {
+                        get_path_u(uuid).then((path) => {
+                            resolve(path);
+                        });
+                    } else {
+                        resolve(json.path);
+                    }
+                });
+        }, 1000);
+    });
+}
+
+// gets path to voice file from voice and text
+async function get_path_vt(voice, text) {
+    let uuid = await get_uuid_vt(voice, text);
+    let path = await get_path_u(uuid);
+    return path;
+}
+
+// export get_voice_file function
+module.exports.get_path_vt = get_path_vt;
